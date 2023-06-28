f4c38afd5e087f80821a18595e3e6ea1efda42d3
George <george-andreas@hotmail.com>
2023-06-27 17:53:30 -0700

    added comment on most_comment_label function

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ def most_common_label(y: ArrayLike):
     """
     Returns the most common label from feature [y]
     """
-    
-    return Counter(y).most_common(1)[0][0]
-    # return st.mode(y, keepdims = False).mode
-    # return np.bincount(y).argmax()
+    return Counter(y).most_common(1)[0][0] # Verstaile. Faster than scipy only if y is not a numpy array
+    # return st.mode(y, keepdims = False).mode # Much Faster than Counter if y is a numpy array, otherwise slower
+    # return np.bincount(y).argmax() # Fastest, best with numpy array. only works if none of the value of y is very large. 
