9e6a8ceb3a8645a8106a7eb49db5aad236a896ca
George <george-andreas@hotmail.com>
2023-07-02 18:16:38 -0700

    updated comments

--- utils/utils.py
+++ utils/utils.py
@@ -8,4 +8,4 @@ def most_common_label(y: ArrayLike):
     """
     return Counter(y).most_common(1)[0][0] # Verstaile. Faster than scipy only if y is not a numpy array
     # return st.mode(y, keepdims = False).mode # Much Faster than Counter if y is a numpy array, otherwise slower
-    # return np.bincount(y).argmax() # Fastest, best with numpy array. only works if none of the value of y is very large. 
+    # return np.bincount(y).argmax() # Fastest, best with numpy array. only works if y is an array of integers with values that aren't large 
