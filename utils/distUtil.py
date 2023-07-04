a29c8f0176cb1546b94185364ff6ccd2189af440
George <george-andreas@hotmail.com>
2023-07-03 20:03:37 -0700

    updated docstring

--- utils/distUtil.py
+++ utils/distUtil.py
@@ -5,11 +5,13 @@ from typing import Optional
 def euclidean(x1: ArrayLike, x2: ArrayLike, axis: Optional[int | tuple[int]] = None) -> float:
     """
     Calculates the euclidean distance between [X1] and [X2] along the [axis]
+    where axis is perform during the summation 
     """
     return np.sqrt(np.sum((x1 - x2) ** 2, axis=axis))
 
 def manhattan(x1: ArrayLike, x2: ArrayLike, axis: Optional[int | tuple[int]] = None):
     """
     Calculates the euclidean distance between [X1] and [X2] along the [axis]
+    where axis is perform during the summation 
     """
     return np.absolute(np.sum((x1 - x2), axis=axis))
