d3d4e6856f15f1e8b7b7feba16366f8168ce4105
George <george-andreas@hotmail.com>
2023-07-12 12:14:25 -0700

    added bootstrap samples function

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.typing import ArrayLike
 from collections import Counter
+from typing import Optional
 
 def most_common_label(y: ArrayLike):
     """
@@ -9,3 +10,32 @@ def most_common_label(y: ArrayLike):
     return Counter(y).most_common(1)[0][0] # Verstaile. Faster than scipy only if y is not a numpy array
     # return st.mode(y, keepdims = False).mode # Much Faster than Counter if y is a numpy array, otherwise slower
     # return np.bincount(y).argmax() # Fastest, best with numpy array. only works if y is an array of integers with values that aren't large 
+
+def obtain_bootstrap_idxs(X: ArrayLike, rng: Optional[np.random.Generator] = None):
+    """
+    Obtain bootstrap indexes samples from dataset ([X], y)
+    
+    Returns:
+        idxs: indexes used for bootstrap samples
+        oob_idxs: indexes used for OOB (Out-Of-Bag)
+    """
+    if rng is None:
+        rng = np.random.default_rng()
+        
+    n_samples = X.shape[0]
+    idxs = rng.choice(n_samples, n_samples, replace = True)
+    oob_idxs = np.setdiff1d(np.arange(n_samples), idxs)
+    return idxs, oob_idxs
+
+def obtain_bootstrap_samples(X: ArrayLike, y: ArrayLike, rng: Optional[np.random.Generator] = None):
+    """
+    Obtain bootstrap samples from dataset ([X], y)
+    
+    Returns:
+        X_bootstrap: bootstrap samples from X
+        X_oob: OOB (Out-Of-Bag) samples from X
+        y_bootstrap: bootstrap samples from Y
+        y_oob: OOB (Out-Of-Bag) samples from Y
+    """
+    idxs, oob_idxs = obtain_bootstrap_idxs(X, rng)
+    return X[idxs], X[oob_idxs], y[idxs], y[oob_idxs]
