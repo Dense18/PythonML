776c751f702d27469723224251e7e325dc5ee097
George <george-andreas@hotmail.com>
2023-07-13 17:55:39 -0700

    added mse and rmse on metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,10 @@
+from typing import Optional
+
 import numpy as np
 from numpy.typing import ArrayLike
+
 from utils.validation import check_consistent_length
-from typing import Optional
+
 
 def euclidean(x1: ArrayLike, x2: ArrayLike, axis: Optional[int | tuple[int]] = None) -> float:
     """
@@ -26,3 +29,39 @@ def accuracy_score(y_test: ArrayLike, y_pred: ArrayLike, normalize: bool = True)
     check_consistent_length(y_test, y_pred)
     score = y_test == y_pred
     return np.average(score) if normalize else score
+
+def mse(y_test: ArrayLike, y_pred: ArrayLike, squared: bool = True) -> float:
+    """
+    Calculates Mean Squared Error value
+    
+    Args:
+    --------
+    y_test: Ground truth target values
+    
+    y_pred: Pedicted target values
+    
+    squared:
+        If True, return MSE, else returns RMSE
+    """
+    check_consistent_length(y_test, y_pred)
+    err =  np.average((y_test - y_pred) ** 2)
+    
+    if not squared:
+        err = np.sqrt(err)
+    
+    return err
+
+def rmse(y_test: ArrayLike, y_pred: ArrayLike) -> float:
+    """
+    Calculates Root Mean Squared Error value
+    
+    Convenience function, similar to calling :term:'mse' function
+    with 'squared' param set to False
+    
+    Args:
+    --------
+    y_test: Ground truth target values
+    
+    y_pred: Pedicted target values
+    """
+    return mse(y_test, y_pred, squared = False)
