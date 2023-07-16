aeeff3290d401b9bf5f641d1c6c9aa0ed73d7581
George <george-andreas@hotmail.com>
2023-07-16 04:19:05 -0700

    Raise Exception on LinearRegression fit method
    Raise an Linalg Exception if the provided X is not invertible

--- LinearRegression.py
+++ LinearRegression.py
@@ -33,6 +33,18 @@ class LinearRegression(SupervisedModel):
     def fit(self, X: ArrayLike, y: ArrayLike):
         """
         Fit Linear Regression using Ordinary Least Squares 
+        
+        Raises
+        --------
+        ValueError:
+            if X or Y have invalid values or shapes
+            
+        LinAlgError: 
+            if a singular matrix is found during fitting process
+            
+            This could be due to:
+            - Linear dependency among the features
+            - n rows < nfeatures
         """
         # Formula used: coef = inverse(X'X)X'y
         # Based on https://online.stat.psu.edu/stat462/node/132/
@@ -43,7 +55,14 @@ class LinearRegression(SupervisedModel):
         if self.fit_intercept:
             copy_X = np.c_[np.ones(X.shape[0]), copy_X]
         
-        inv = np.linalg.inv(np.dot(copy_X.T, copy_X)) # inverse(X'X)
+        try:
+            inv = np.linalg.inv(np.dot(copy_X.T, copy_X)) # inverse(X'X)
+        except Exception as err:
+            raise np.linalg.LinAlgError(
+                   "Found Singularity matrix during fitting process.\nThis could be due to:\n"
+                   "  - Linear dependency among the features\n"
+                   "  - n_rows < n_features.\n"
+                ) from err
         X_t_y = np.dot(copy_X.T, y) # X'y
         coef = np.dot(inv, X_t_y) # inverse(X'X)X'y
         self.bias, self.weights = coef[0], coef[1:]
